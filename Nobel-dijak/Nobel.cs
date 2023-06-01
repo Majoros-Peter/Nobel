002bee24a729e7c8294b2817993e1b5e66bf4bf6
Majoros-Peter <majorospeterur@gmail.com>
2023-06-01 10:54:16 +0200

    Kész.

---
+++ Nobel-dijak/Nobel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Nobel_dijak
+{
+    internal class Nobel
+    {
+        public int Evszam { get; private set; }
+        public string Tipus { get; private set; }
+        public string Keresztnev { get; private set; }
+        public string Vezeteknev { get; private set; }
+
+        public Nobel(string[] adatok)
+        {
+            Evszam = Convert.ToInt32(adatok[0]);
+            Tipus = adatok[1];
+            Keresztnev = adatok[2];
+            Vezeteknev = adatok[3];
+        }
+        public string TeljesNev { get => $"{Keresztnev} {Vezeteknev}"; }
+
+        public override string ToString() => $"{Evszam};{TeljesNev}";
+    }
+}
--- Nobel-dijak/Program.cs
+++ Nobel-dijak/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Nobel_dijak
 {
@@ -10,6 +11,18 @@ namespace Nobel_dijak
     {
         static void Main(string[] args)
         {
+            List<Nobel> nobel = File.ReadAllLines("nobel.csv").Skip(1).Select(G=> new Nobel(G.Split(';'))).ToList();
+
+            Console.WriteLine($"4. feladat: {nobel.Where(G=>G.Evszam == 2017 && G.Tipus == "irodalmi").First().TeljesNev}");
+
+            Console.WriteLine("6. feladat:");
+            nobel.Where(G => G.TeljesNev.Contains("Curie")).ToList().ForEach(G=> Console.WriteLine($"\t{G.TeljesNev}({G.Tipus})"));
+
+            Console.WriteLine("7. feladat:");
+            nobel.GroupBy(G => G.Tipus).ToList().ForEach(G => Console.WriteLine($"\t{G.Key.PadRight(30)}{G.Count().ToString().PadLeft(3)} db"));
+
+            File.WriteAllLines("orvosi.txt", nobel.Where(G => G.Tipus == "orvosi").Select(G => G.ToString()).ToList());
+            Console.WriteLine("8. feladat: orvosi.txt");
         }
     }
 }
